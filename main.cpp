0c21805b389863c096040a080c0281fa3957c69e
adelisa <adelisa-mujaric@outlook.com>
2024-08-16 13:27:57 +0200

    commited

--- main.cpp
+++ main.cpp
@@ -3,6 +3,37 @@ using namespace std;
 
 int main(){
 
+    double num1 , num2;
+    string operation;
+
+    cout<< "Enter first number: "; cin>> num1; 
+    cout << "Enter second number: ";cin>>num2;
+    cout <<"Choose your operation: \n + for addition\n - for subtracktion\n * for multiplication\n / for division"<<endl; 
+    cout <<"You choose? --> "; cin>> operation;   
+
+    double result;
+    if (operation == "+")
+    {
+        result = num1 + num2;
+    }else if(operation == "-"){
+        result = num1 - num2;
+    }else if(operation == "*"){
+        result = num1 * num2;
+    }else if(operation == "/"){
+        if(num2 == 0){
+            cout<< "You can not divide anything with zero (0)"<<endl;
+            }
+        else
+            result = num1 / num2;
+
+    }else{
+        cout<<"You can only type +, -, * or  / --> " <<endl;
+        cout <<"You choose? --> "; cin>> operation;   
+
+    }
+
+    cout << "Your result is: "<< result << endl; 
+     
     system("pause>0");
     return 0;
 }
