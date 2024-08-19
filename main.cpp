16b7e6cb53ff3641ac2adbdec0caf9235ed9faf1
adelisa <adelisa-mujaric@outlook.com>
2024-08-19 20:18:31 +0200

    issues fixed

--- main.cpp
+++ main.cpp
@@ -8,28 +8,42 @@ int main(){
 
     cout<< "Enter first number: "; cin>> num1; 
     cout << "Enter second number: ";cin>>num2;
-    cout <<"Choose your operation: \n + for addition\n - for subtracktion\n * for multiplication\n / for division"<<endl; 
-    cout <<"You choose? --> "; cin>> operation;   
 
     double result;
-    if (operation == "+")
-    {
-        result = num1 + num2;
-    }else if(operation == "-"){
-        result = num1 - num2;
-    }else if(operation == "*"){
-        result = num1 * num2;
-    }else if(operation == "/"){
-        if(num2 == 0){
-            cout<< "You can not divide anything with zero (0)"<<endl;
-            }
-        else
-            result = num1 / num2;
+    bool validOperation = false;
 
-    }else{
-        cout<<"You can only type +, -, * or  / --> " <<endl;
-        cout <<"You choose? --> "; cin>> operation;   
+    while(!validOperation){
 
+        cout <<"Choose your operation: \n + for addition\n - for subtracktion\n * for multiplication\n / for division"<<endl; 
+        cout <<"You choose? --> "; cin>> operation; 
+
+        if (operation == "+")
+        {
+            result = num1 + num2;
+            validOperation = true;
+        }else if(operation == "-"){
+            result = num1 - num2;
+            validOperation = true;
+        }else if(operation == "*"){
+            result = num1 * num2;
+            validOperation = true;
+        }else if(operation == "/"){
+                if(num2 == 0){
+                    cout<<"You can not divide anything by zero, numbers switched positions" <<endl;
+                    double temp = num2;
+                    num2 = num1;
+                    num1 = temp;
+                    cout<< "num1 = " << num1 << ", num2 = "<< num2 << endl;
+
+                    result = num1 / num2;
+                    validOperation = true;
+                }else{
+                    result = num1 / num2;
+                    validOperation = true;
+                }
+        }else{
+            cout<<"------ You can only type +, -, * or  /------" <<endl;
+        }
     }
 
     cout << "Your result is: "<< result << endl; 
